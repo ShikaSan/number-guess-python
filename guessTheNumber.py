585c257532596ff22915e259b73953adbf7abba2
JV Lorilla <jay.lorilla27@gmail.com>
2020-03-11 14:08:05 +0800

    Update guessTheNumber.py
    Updated and added some functions.

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -2,7 +2,10 @@
 
 import random # Import this module for generating a random number
 
-secretNumber = random.randint(1, 20) # Set the random number as a global variable
+# Function to generate a random number
+def generateRandom_Num():
+    randomNum = random.randint(1, 20)
+    return randomNum
 
 # Function to determine if answer is too low, too high, or if it is correct
 def guessFunc(guessInput, randomNum):
@@ -14,25 +17,32 @@ def guessFunc(guessInput, randomNum):
         return 'Correct!'
 
 # The "main" function of the whole game
-def mainFunc(randomGen_Number):
+def mainFunc():
     userGuess = ''
     guessResult = ''
+    getRandom_Num = generateRandom_Num()
 
-    # Game loop, the program ends if user types 'Exit'/'exit' or guesses the correct answer.
+    # Game loop, the program ends if user types 'Exit'/'exit'.
     while True:
         print('I\'m thinking of a number between 0 and 20...')
         userGuess = input()
         
         if userGuess=='Exit' or userGuess=='exit':
-            return
+            return 
         else:
-            guessResult = guessFunc(int(userGuess), randomGen_Number)
+            guessResult = guessFunc(int(userGuess), getRandom_Num)
             print(guessResult + '\n')
             
         if guessResult == 'Correct!':
-            return
+            loopMain()
+            
+# If the answer is correct, call main function again, so that program won't close.
+def loopMain():
+    mainFunc()
 
 # Initialize/call main function       
-mainFunc(secretNumber)
+mainFunc()
+
+
 
     
