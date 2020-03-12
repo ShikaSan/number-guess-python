cf5e61d72b4d7fcf8e881c7d2901618050df81ef
JV Lorilla <jay.lorilla27@gmail.com>
2020-03-12 09:12:51 +0800

    Updated user input error checking
    Made some changes to the user input error checking.

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -16,6 +16,13 @@ def guessFunc(guessInput, randomNum):
     else:
         return 'Correct!'
 
+# Function that checks if user input is valid.
+def checkValid_Input(userInput):
+    if userInput.isdigit():
+        return True
+
+    return False;
+
 # The "main" function of the whole game
 def mainFunc():
     userGuess = ''
@@ -26,19 +33,24 @@ def mainFunc():
     while True:
         print('I\'m thinking of a number between 0 and 20...')
         userGuess = input()
+
+        # Check first if user typed 'exit'. If true, terminates the program
+        # if False, continues code execution.
+        if str.lower(userGuess) == 'exit':
+            return;
         
-        if userGuess=='Exit' or userGuess=='exit':
-            return 
+        # If the previous condition is false, perform this method.
+        # This checks if user input is valid.
+        isValid = checkValid_Input(userGuess)
+        
+        if isValid==False:
+            print('Invalid input, please try again.\n')
         else:
             guessResult = guessFunc(int(userGuess), getRandom_Num)
             print(guessResult + '\n')
             
-        if guessResult == 'Correct!':
-            return loopMain()
-            
-# If the answer is correct, call main function again, so that program won't close.
-def loopMain():
-    return mainFunc()
+            if guessResult == 'Correct!':
+                    return mainFunc()
 
 # Initialize/call main function       
 mainFunc()
